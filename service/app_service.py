287b18d9f12e007032ceb99bb4449c56912435b6
dgherghel <ghergheldenisa@gmail.com>
2020-01-06 09:42:30 +0200

    Merged: 3D Renderer + refactoring + layout changes

---
+++ service/app_service.py
@@ -0,0 +1,88 @@
+from unet.unet3DModelWithGenerator import Unet3DModelWithGenerator
+from renderer.renderer3d import Renderer3D
+
+import utils.utils as myUtils
+
+
+class AppService:
+    def __init__(self):
+        self._image_path = ""
+        self._label_path = ""
+        self._3d_mask_path = ""
+        self._model = None
+        self._renderer = Renderer3D()
+        self._on_image_change_callback = None
+
+        self._init_model()
+
+    def set_image_path(self, image_path):
+        self._image_path = image_path
+
+        # when loading a new nifti image, the generated labels are not corresponding anymore
+        self._label_path = ""
+        self._3d_mask_path = ""
+
+        if self._on_image_change_callback is not None:
+            self._on_image_change_callback(self._label_path, self._3d_mask_path)
+
+    def get_image_path(self):
+        return self._image_path
+
+    def _init_model(self):
+        self._model = Unet3DModelWithGenerator([((0), "Background"), ((127), "Ventricular Myocardum"),
+                                               ((255), "Blood Pool")],
+                                               64)
+        self._model.load_model()
+
+    def generate_mask(self, get_path_for_saving_callback, labels_generated_callback, rendered_mask_generated_callback):
+        if self._image_path == "":
+            print("ERROR: There is no nifti image opened. Cannot generate labels!")
+            return
+
+        print("INFO: Generate labels")
+
+        image_data, affine = myUtils.load_nifti_image(self._image_path)
+
+        generated_mask = self._model.predict_volume(image_data)
+
+        self._save_main_mask(generated_mask, affine, get_path_for_saving_callback, labels_generated_callback)
+        self._save_3d_rendered_mask(generated_mask, get_path_for_saving_callback, rendered_mask_generated_callback)
+
+    def _save_main_mask(self, image_to_save, affine, get_path_for_saving_callback, labels_generated_callback):
+        path = get_path_for_saving_callback(".nii")
+        if path is None:
+            print("WARN: Labels were not saved due to no path or invalid path provided! Please generate again!")
+            return
+        try:
+            myUtils.save_nifti_image(image_to_save, affine, path)
+            self._label_path = path
+            labels_generated_callback(path)
+            print("INFO: Labels were successfully generated and saved!")
+        except Exception:
+            print("ERROR: Labels could not be saved! Please generate again!")
+
+    def _save_3d_rendered_mask(self, image, get_path_for_saving_callback, rendered_mask_generated_callback):
+        iframe_to_save = self._renderer.generate(image)
+        path = get_path_for_saving_callback(".html")
+        if path is None:
+            print(
+                "WARN: 3d rendered mask was not saved due to no path or invalid path provided! Please generate again!")
+            return
+        iframe_to_save.write_html(path)
+        self._3d_mask_path = path
+        rendered_mask_generated_callback(path)
+        print("INFO: 3d rendered mask was successfully generated and saved!")
+
+    def subscribe_to_image_changes(self, callback):
+        self._on_image_change_callback = callback
+
+    def get_label_path(self):
+        return self._label_path
+
+    def set_labels_path(self, path):
+        self._label_path = path
+        if self._on_image_change_callback is not None:
+            self._on_image_change_callback(self._label_path, self._3d_mask_path)
+
+    def get_3d_mask_path(self):
+        return self._3d_mask_path
--- unet/NIfTI2DPreprocessor.py
+++ unet/NIfTI2DPreprocessor.py
@@ -33,8 +33,8 @@ class NIfTI2DPreprocessor:
         os.makedirs(self.PREPROCESSED_TRAIN_PATH + 'masks/', exist_ok=True)
         os.makedirs(self.PREPROCESSED_TEST_PATH + 'images/', exist_ok=True)
         os.makedirs(self.PREPROCESSED_TEST_PATH + 'masks/', exist_ok=True)
-    
-    def preprocess( self ):
+
+    def preprocess(self):
         # Get train and test IDs
         # Returns a list of image file names in the training and testing paths
         # Label files are expected to be image_name -label
@@ -44,130 +44,133 @@ class NIfTI2DPreprocessor:
         print('Getting and resizing training images and masks... ', flush=True)
         for n, id_ in enumerate(train_image_ids):
             # Process images
-            print( "Exporting image %d/%d" % ( n+1, len(train_image_ids) ), flush=True )
+            print("Exporting image %d/%d" % (n + 1, len(train_image_ids)), flush=True)
 
             image_data, _ = myUtils.load_nifti_image( self.TRAIN_PATH + 'images/' + id_)
-            min_max = ( image_data.min(), image_data.max() )
+            min_max = (image_data.min(), image_data.max())
 
-            numslices0 = image_data.shape[ 0 ]
-            numslices1 = image_data.shape[ 1 ]
-            numslices2 = image_data.shape[ 2 ]
-           
+            numslices0 = image_data.shape[0]
+            numslices1 = image_data.shape[1]
+            numslices2 = image_data.shape[2]
+
             name = id_.split('.')[0]
 
-            for i in tqdm( range(numslices0) ):
-                img = image_data[i,:,:]
-                img = np.flip( img.T, axis = 0 )
-                if len( img.shape ) == 2 or ( self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS ):
-                    img = gray2rgb( img )
-                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True )
-                img = img * ( 255 / min_max[1] )
+            for i in tqdm(range(numslices0)):
+                img = image_data[i, :, :]
+                img = np.flip(img.T, axis=0)
+                if len(img.shape) == 2 or (self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS):
+                    img = gray2rgb(img)
+                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True)
+                img = img * (255 / min_max[1])
                 img = img.astype(np.uint8)
-                imsave( self.PREPROCESSED_TRAIN_PATH + "images/AXIS0-%s-%04d.png" % (name, i), img )
+                imsave(self.PREPROCESSED_TRAIN_PATH + "images/AXIS0-%s-%04d.png" % (name, i), img)
 
             for j in tqdm(range(numslices1)):
-                img = image_data[:,j,:]
-                img = np.flip( img.T, axis = 0 )
-                if len( img.shape ) == 2 or ( self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS ):
-                    img = gray2rgb( img )
+                img = image_data[:, j, :]
+                img = np.flip(img.T, axis=0)
+                if len(img.shape) == 2 or (self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS):
+                    img = gray2rgb(img)
                 img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True)
-                img = img * ( 255 / min_max[1] )
+                img = img * (255 / min_max[1])
                 img = img.astype(np.uint8)
-                imsave( self.PREPROCESSED_TRAIN_PATH + "images/AXIS1-%s-%04d.png" % (name, j), img )
+                imsave(self.PREPROCESSED_TRAIN_PATH + "images/AXIS1-%s-%04d.png" % (name, j), img)
 
             for k in tqdm(range(numslices2)):
-                img = image_data[:,:,k]
-                img = np.flip( img.T, axis = 0 )
-                if len( img.shape ) == 2 or ( self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS ):
-                    img = gray2rgb( img )
+                img = image_data[:, :, k]
+                img = np.flip(img.T, axis=0)
+                if len(img.shape) == 2 or (self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS):
+                    img = gray2rgb(img)
                 img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True)
-                img = img * ( 255 / min_max[1] )
+                img = img * (255 / min_max[1])
                 img = img.astype(np.uint8)
-                imsave( self.PREPROCESSED_TRAIN_PATH + "images/AXIS2-%s-%04d.png" % (name, k), img )
+                imsave(self.PREPROCESSED_TRAIN_PATH + "images/AXIS2-%s-%04d.png" % (name, k), img)
 
             # Process masks
             image_data, _ = myUtils.load_nifti_image( self.TRAIN_PATH + 'masks/' + name + "-label.nii.gz")
             min_max = ( image_data.min(), image_data.max() )
-           
-            numslices0 = image_data.shape[ 0 ]
-            numslices1 = image_data.shape[ 1 ]
-            numslices2 = image_data.shape[ 2 ]
-           
+
+            numslices0 = image_data.shape[0]
+            numslices1 = image_data.shape[1]
+            numslices2 = image_data.shape[2]
+
             name = id_.split('.')[0].split('-')[0]
 
             for i in tqdm(range(numslices0)):
-                img = image_data[i,:,:]
-                img = np.flip( img.T, axis = 0 )
-                if len( img.shape ) == 2 or ( self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS ):
-                    img = gray2rgb( img )
-                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True, order = 0, anti_aliasing = False)
-                img = img * ( 255 / min_max[1] )
+                img = image_data[i, :, :]
+                img = np.flip(img.T, axis=0)
+                if len(img.shape) == 2 or (self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS):
+                    img = gray2rgb(img)
+                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True, order=0,
+                             anti_aliasing=False)
+                img = img * (255 / min_max[1])
                 img = img.astype(np.uint8)
-                imsave( self.PREPROCESSED_TRAIN_PATH + "masks/AXIS0-%s-%04d.png" % (name, i), img )
+                imsave(self.PREPROCESSED_TRAIN_PATH + "masks/AXIS0-%s-%04d.png" % (name, i), img)
 
             for j in tqdm(range(numslices1)):
-                img = image_data[:,j,:]
-                img = np.flip( img.T, axis = 0 )
-                if len( img.shape ) == 2 or ( self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS ):
-                    img = gray2rgb( img )
-                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True, order = 0, anti_aliasing = False)
-                img = img * ( 255 / min_max[1] )
+                img = image_data[:, j, :]
+                img = np.flip(img.T, axis=0)
+                if len(img.shape) == 2 or (self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS):
+                    img = gray2rgb(img)
+                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True, order=0,
+                             anti_aliasing=False)
+                img = img * (255 / min_max[1])
                 img = img.astype(np.uint8)
-                imsave( self.PREPROCESSED_TRAIN_PATH + "masks/AXIS1-%s-%04d.png" % (name, j), img )
+                imsave(self.PREPROCESSED_TRAIN_PATH + "masks/AXIS1-%s-%04d.png" % (name, j), img)
 
             for k in tqdm(range(numslices2)):
-                img = image_data[:,:,k]
-                img = np.flip( img.T, axis = 0 )
-                if len( img.shape ) == 2 or ( self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS ):
-                    img = gray2rgb( img )
-                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True, order = 0, anti_aliasing = False)
-                img = img * ( 255 / min_max[1] )
+                img = image_data[:, :, k]
+                img = np.flip(img.T, axis=0)
+                if len(img.shape) == 2 or (self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS):
+                    img = gray2rgb(img)
+                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True, order=0,
+                             anti_aliasing=False)
+                img = img * (255 / min_max[1])
                 img = img.astype(np.uint8)
-                imsave( self.PREPROCESSED_TRAIN_PATH + "masks/AXIS2-%s-%04d.png" % (name, k), img )
+                imsave(self.PREPROCESSED_TRAIN_PATH + "masks/AXIS2-%s-%04d.png" % (name, k), img)
 
         print('Getting and resizing testing images and masks... ', flush=True)
         for n, id_ in enumerate(test_image_ids):
             # Process images
-            print( "Exporting image %d/%d" % ( n+1, len(test_image_ids) ), flush=True )
+            print("Exporting image %d/%d" % (n + 1, len(test_image_ids)), flush=True)
 
             image_data, _ = myUtils.load_nifti_image( self.TEST_PATH + 'images/' + id_)
             min_max = ( image_data.min(), image_data.max() )
 
-            numslices0 = image_data.shape[ 0 ]
-            numslices1 = image_data.shape[ 1 ]
-            numslices2 = image_data.shape[ 2 ]
+            numslices0 = image_data.shape[0]
+            numslices1 = image_data.shape[1]
+            numslices2 = image_data.shape[2]
 
             name = id_.split('.')[0]
 
-            for i in tqdm( range(numslices0) ):
-                img = image_data[i,:,:]
-                img = np.flip( img.T, axis = 0 )
-                if len( img.shape ) == 2 or ( self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS ):
-                    img = gray2rgb( img )
-                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True )
-                img = img * ( 255 / min_max[1] )
+            for i in tqdm(range(numslices0)):
+                img = image_data[i, :, :]
+                img = np.flip(img.T, axis=0)
+                if len(img.shape) == 2 or (self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS):
+                    img = gray2rgb(img)
+                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True)
+                img = img * (255 / min_max[1])
                 img = img.astype(np.uint8)
-                imsave( self.PREPROCESSED_TEST_PATH + "images/AXIS0-%s-%04d.png" % (name, i), img )
+                imsave(self.PREPROCESSED_TEST_PATH + "images/AXIS0-%s-%04d.png" % (name, i), img)
 
             for j in tqdm(range(numslices1)):
-                img = image_data[:,j,:]
-                img = np.flip( img.T, axis = 0 )
-                if len( img.shape ) == 2 or ( self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS ):
-                    img = gray2rgb( img )
+                img = image_data[:, j, :]
+                img = np.flip(img.T, axis=0)
+                if len(img.shape) == 2 or (self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS):
+                    img = gray2rgb(img)
                 img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True)
-                img = img * ( 255 / min_max[1] )
+                img = img * (255 / min_max[1])
                 img = img.astype(np.uint8)
-                imsave( self.PREPROCESSED_TEST_PATH + "images/AXIS1-%s-%04d.png" % (name, j), img )
+                imsave(self.PREPROCESSED_TEST_PATH + "images/AXIS1-%s-%04d.png" % (name, j), img)
 
             for k in tqdm(range(numslices2)):
-                img = image_data[:,:,k]
-                img = np.flip( img.T, axis = 0 )
-                if len( img.shape ) == 2 or ( self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS ):
-                    img = gray2rgb( img )
+                img = image_data[:, :, k]
+                img = np.flip(img.T, axis=0)
+                if len(img.shape) == 2 or (self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS):
+                    img = gray2rgb(img)
                 img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True)
-                img = img * ( 255 / min_max[1] )
+                img = img * (255 / min_max[1])
                 img = img.astype(np.uint8)
-                imsave( self.PREPROCESSED_TEST_PATH + "images/AXIS2-%s-%04d.png" % (name, k), img )
+                imsave(self.PREPROCESSED_TEST_PATH + "images/AXIS2-%s-%04d.png" % (name, k), img)
 
             if not self.TEST_LABELED:
                 continue
@@ -176,38 +179,41 @@ class NIfTI2DPreprocessor:
             image_data, _ = myUtils.load_nifti_image( self.TEST_PATH + 'masks/' + name + "-label.nii.gz")
             min_max = ( image_data.min(), image_data.max() )
 
-            numslices0 = image_data.shape[ 0 ]
-            numslices1 = image_data.shape[ 1 ]
-            numslices2 = image_data.shape[ 2 ]
+            numslices0 = image_data.shape[0]
+            numslices1 = image_data.shape[1]
+            numslices2 = image_data.shape[2]
 
             name = id_.split('.')[0].split('-')[0]
 
             for i in tqdm(range(numslices0)):
-                img = image_data[i,:,:]
-                img = np.flip( img.T, axis = 0 )
-                if len( img.shape ) == 2 or ( self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS ):
-                    img = gray2rgb( img )
-                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True, order = 0, anti_aliasing = False)
-                img = img * ( 255 / min_max[1] )
+                img = image_data[i, :, :]
+                img = np.flip(img.T, axis=0)
+                if len(img.shape) == 2 or (self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS):
+                    img = gray2rgb(img)
+                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True, order=0,
+                             anti_aliasing=False)
+                img = img * (255 / min_max[1])
                 img = img.astype(np.uint8)
-                imsave( self.PREPROCESSED_TEST_PATH + "masks/AXIS0-%s-%04d.png" % (name, i), img )
+                imsave(self.PREPROCESSED_TEST_PATH + "masks/AXIS0-%s-%04d.png" % (name, i), img)
 
             for j in tqdm(range(numslices1)):
-                img = image_data[:,j,:]
-                img = np.flip( img.T, axis = 0 )
-                if len( img.shape ) == 2 or ( self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS ):
-                    img = gray2rgb( img )
-                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True, order = 0, anti_aliasing = False)
-                img = img * ( 255 / min_max[1] )
+                img = image_data[:, j, :]
+                img = np.flip(img.T, axis=0)
+                if len(img.shape) == 2 or (self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS):
+                    img = gray2rgb(img)
+                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True, order=0,
+                             anti_aliasing=False)
+                img = img * (255 / min_max[1])
                 img = img.astype(np.uint8)
-                imsave( self.PREPROCESSED_TEST_PATH + "masks/AXIS1-%s-%04d.png" % (name, j), img )
+                imsave(self.PREPROCESSED_TEST_PATH + "masks/AXIS1-%s-%04d.png" % (name, j), img)
 
             for k in tqdm(range(numslices2)):
-                img = image_data[:,:,k]
-                img = np.flip( img.T, axis = 0 )
-                if len( img.shape ) == 2 or ( self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS ):
-                    img = gray2rgb( img )
-                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True, order = 0, anti_aliasing = False)
-                img = img * ( 255 / min_max[1] )
+                img = image_data[:, :, k]
+                img = np.flip(img.T, axis=0)
+                if len(img.shape) == 2 or (self.IMG_CHANNELS == 3 and img.shape[2] != self.IMG_CHANNELS):
+                    img = gray2rgb(img)
+                img = resize(img, (self.IMG_HEIGHT, self.IMG_WIDTH), mode='constant', preserve_range=True, order=0,
+                             anti_aliasing=False)
+                img = img * (255 / min_max[1])
                 img = img.astype(np.uint8)
-                imsave( self.PREPROCESSED_TEST_PATH + "masks/AXIS2-%s-%04d.png" % (name, k), img )
+                imsave(self.PREPROCESSED_TEST_PATH + "masks/AXIS2-%s-%04d.png" % (name, k), img)
